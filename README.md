b7c76f7265fe18bdcf68546bf1b81f2bf895f689
William Stewart <hmaswill@gmail.com>
2018-06-24 17:26:17 +1200

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # no-eu
 Express middleware that blocks all EU traffic
+
+## Install
+```npm install no-eu --save```
+
+## Usage
+```
+const eu = require("no-eu");
+const express = require('express');
+const app = express();
+
+app.use(eu( (req, res, location) => {
+    console.log(location);
+    res.send("Sorry you're in the EU");
+}));
+```
---
+++ index.js
@@ -0,0 +1,19 @@
+module.exports = function(callback) {
+    return function(req, res, next) {
+        const getIP = require('ipware')().get_ip;
+        const geolite2 = require('geolite2');
+        const maxmind = require('maxmind');
+        const eu = require("./eu-members");
+        const lookup = maxmind.openSync(geolite2.paths.country);
+        let ip = getIP(req).clientIp;
+        let location = lookup.get(ip).country.iso_code;
+        
+        if(eu.includes(location)) {
+            // Reject request
+            return callback(req, res, location);
+        } else {
+            // Accept request
+            next();
+        }
+    }
+}
